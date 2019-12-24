b79a092cd0e3d2d1dd64525358ccdab0d279cf93
Chad <stefflc@msaerospace.com>
2019-12-24 10:12:34 -0800

    LDAP Authentication
    Add LDAP authentication, add db.sqlite3 to the gitignore list so the data doesn't become dirty.

--- PP/settings.py
+++ PP/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'jquery',
+    'django_python3_ldap', #Active Directory authentication
     'rest_framework',
     'import_export',
     'django.contrib.humanize',
@@ -46,7 +47,8 @@ INSTALLED_APPS = [
     'DepartmentLog',
     'Reportview',
     'LABLog',
-    
+    'django.contrib.admin', ##This must be after 'TrainingLog' so logout process TrainingLog logged_out.html, and not Django Admin logout
+
 ]
 
 IMPORT_EXPORT_USE_TRANSACTIONS = True
@@ -110,6 +112,49 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+        
+        "django_python3_ldap.auth.LDAPBackend", ###If you want Active Directory users to be able to log in
+    "django.contrib.auth.backends.ModelBackend", ###If you want Django users to be able to log in 
+        ]
+
+LDAP_AUTH_FORMAT_USERNAME = "django_python3_ldap.utils.format_username_active_directory"
+LDAP_AUTH_ACTIVE_DIRECTORY_DOMAIN = "MSAEROSPACE" ##Works needed for msaerospace\username login scheme
+
+
+##Can be foudn in DC2 - > Active Directory Users and Computers -> Users -> pick a user -> Attribut Editor
+LDAP_AUTH_USER_FIELDS = {
+    "username": "sAMAccountName", #correct
+    "first_name": "givenName", #correct
+    "last_name": "sn", #correct
+    "email": "mail", #correct
+}
+
+LDAP_AUTH_OBJECT_CLASS = "users"
+LDAP_AUTH_USER_LOOKUP_FIELDS = ('username',)
+
+
+
+#LDAP_AUTH_URL = 'ldap://DC2.msaerospace.local:389' ## SEEMS TO BE WORKING; 
+#LDAP_AUTH_URL = 'ldap://DC2:389' ## SEEMS TO BE WORKING; do not use port 389 that sends PW in the clear
+LDAP_AUTH_URL = 'ldap://10.1.1.28:389' ## SEEMS TO BE WORKING; do not use port 389 that sends PW in the clear
+#LDAP_AUTH_URL = 'ldap://10.1.1.29:636' ## NOT WORKING; do not use port 389 that sends PW in the clear
+##From DC2 - > CMD - > dsquery user -name administrator, drop CN=administrator
+#LDAP_AUTH_SEARCH_BASE = "CN=Users, DC=msaerospace,DC=local" ##This no longer works as of 05/28/19 as some users are now separated into departments
+'''This will search top level active directory, parsing through all sub folders. This is the most broad search'''
+LDAP_AUTH_SEARCH_BASE = "DC=msaerospace,DC=local"
+#this is from 10.1.1.24 Active Directory - > Domain Controllers -> Attribute Editor
+#this is from 10.1.1.24 Active Directory - > msaerospace.local side bar -> properties -> attribute editor
+
+#LDAP_AUTH_USE_TLS=True
+LDAP_AUTH_CONNECTION_USERNAME = None
+LDAP_AUTH_CONNECTION_PASSWORD = None
+
+
+
+## DC2 - > Active Director Users and Computers -> Pick a User -> Attribut Editor -> objectClass
+LDAP_AUTH_OBJECT_CLASS = "organizationalPerson"
 
 
 REST_FRAMEWORK = {
